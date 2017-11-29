10605a67874aefaa04459285bb2f8b8c4312ff4e
klxin <xinkailun@itcast.com>
2017-11-29 16:41:31 +0800

    首页更改

--- Interface/slides.php
+++ Interface/slides.php
@@ -3,10 +3,10 @@
  * @Author: klxin
  * @Date:   2017-11-29 16:00:42
  * @Last Modified by:   klxin
- * @Last Modified time: 2017-11-29 16:04:52
+ * @Last Modified time: 2017-11-29 16:37:10
  */
 
 $slides = array();
-$slides[0] = json_encode(array('id' => 0, 'image' => '', 'link': '/pages/list/list?cat=10'));
-$slides[1] = json_encode(array('id' => 0, 'image' => '', 'link': '/pages/list/list?cat=10'));
+$slides[0] = json_encode(array('id' => 0, 'image' => '../images/WeChat/images/banner-01.png', 'link': '/pages/list/list?cat=1'));
+$slides[1] = json_encode(array('id' => 1, 'image' => '../images/WeChat/images/banner-02.png', 'link': '/pages/list/list?cat=2'));
 echo slides;
--- index.php
+++ index.php
@@ -6,5 +6,7 @@
     <body>
         天下隆中对，传奇襄阳城
         <img style="display: none;" src="images/WeChat/images/banner-01.png">
+        <?php echo array('id' => 1); ?>
+        <?php echo json_encode(array('id' => 1)); ?>
     </body>
 </html>
