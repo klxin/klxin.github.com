5bd8f6b0e58d575906c734ba97775f1e67d34369
klxin <xinkailun@itcast.com>
2017-11-29 16:05:27 +0800

    增加小程序图片接口等

---
+++ Interface/slides.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @Author: klxin
+ * @Date:   2017-11-29 16:00:42
+ * @Last Modified by:   klxin
+ * @Last Modified time: 2017-11-29 16:04:52
+ */
+
+$slides = array();
+$slides[0] = json_encode(array('id' => 0, 'image' => '', 'link': '/pages/list/list?cat=10'));
+$slides[1] = json_encode(array('id' => 0, 'image' => '', 'link': '/pages/list/list?cat=10'));
+echo slides;
